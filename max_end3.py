db7591ae3200ece058955c2d9a8c9f4705dced1b
Sameem <sultansameem@gmail.com>
2018-04-15 18:53:14 +0530

    Create max_end3.py

---
+++ max_end3.py
@@ -0,0 +1,17 @@
+# Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+
+#Ex:  max_end3([1, 2, 3]) → [3, 3, 3]
+#     max_end3([11, 5, 9]) → [11, 11, 11]
+#     max_end3([2, 11, 3]) → [3, 3, 3]
+
+def max_end3(nums):
+  max = 0
+  if (nums[0]>nums[-1]):
+    max = nums[0]
+  else:
+    max = nums[-1]
+  
+  for i in range(len(nums)):
+    nums[i] = max
+  
+  return nums
